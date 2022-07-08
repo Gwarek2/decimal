00a8d759a02dc41cffc71ce56caf23b36a725ade
Werlag Deckard <werlagde@student.21-school.ru>
2022-07-08 19:26:30 +0300

    Refactored main header

--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -3,14 +3,15 @@
 
 #include "decimal_type.h"
 
-// Arithmetic operations
+// Arithmetics
 int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 int s21_mod(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
+int s21_negate(s21_decimal value, s21_decimal *result);
 
-// Comparators
+// Comparison
 int s21_is_less(s21_decimal, s21_decimal);
 int s21_is_less_or_equal(s21_decimal, s21_decimal);
 int s21_is_greater(s21_decimal, s21_decimal);
@@ -18,17 +19,15 @@ int s21_is_greater_or_equal(s21_decimal, s21_decimal);
 int s21_is_equal(s21_decimal, s21_decimal);
 int s21_is_not_equal(s21_decimal, s21_decimal);
 
-// Converters
+// Conversion
 int s21_from_int_to_decimal(int src, s21_decimal *dst);
 int s21_from_float_to_decimal(float src, s21_decimal *dst);
 int s21_from_decimal_to_int(s21_decimal src, int *dst);
 int s21_from_decimal_to_float(s21_decimal src, float *dst);
-void decimal_to_string(s21_decimal value, char *buffer);
 
 // Rounding
 int s21_floor(s21_decimal value, s21_decimal *result);
 int s21_round(s21_decimal value, s21_decimal *result);
 int s21_truncate(s21_decimal value, s21_decimal *result);
-int s21_negate(s21_decimal value, s21_decimal *result);
 
 #endif
