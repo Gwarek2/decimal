81b004c77f56ba8bf658efd041609aec1f3686ce
Werlag Deckard <werlagde@student.21-school.ru>
2022-07-18 19:36:54 +0300

    Corrected returned value in case of unsettled overflow

--- src/arithmetics/s21_mul.c
+++ src/arithmetics/s21_mul.c
@@ -11,7 +11,7 @@ int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
     base_multiply(value_1, value_2, result, &overflow);
     if (!is_zero(overflow) &&
         round_result(result, &overflow, &res_scale)) {
-        return DEC_HUGE;
+        return res_sign ? DEC_SMALL : DEC_HUGE;
     }
     while (res_scale > 28) {
         base_bank_rounding(*result, result);
