1fcafdad3abd437ff4da63086a4caa4882d4bb7c
Zona Nada <zonanada@oa-a2.msk.21-school.ru>
2022-07-20 19:34:32 +0300

    ready s21_add without tests

--- src/core/app.c
+++ src/core/app.c
@@ -650,45 +650,62 @@ int round_result_192(uint192 *value, s21_decimal *result, int *scale) {
 int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
     int other_scale = get_scale(value_1) - get_scale(value_2); // разница степеней
     s21_decimal overflow = {0};
-    uint192 with_overflow = {0};
-    uint192 without_overflow = {0};
-    uint192 result_add_192 = {0};
+    uint192 result_192 = {0};
+    int sign_value_1 = get_sign(value_1);
+    int sign_value_2 = get_sign(value_2);
     int result_function = DEC_HUGE;
+    uint192 value_1_192 = {0};
+    uint192 value_2_192 = {0};
 
     alignment_scale(&value_1, &value_2, &overflow); // вырвнивание степеней и запись переполнения
-    my_print(value_1);
-    my_print(value_2);
-    my_print(overflow);
+    convert_to_uint192(other_scale < 0 ? overflow : DEC_ZERO, value_1, &value_1_192); // конвертация числа с переполнением в 192бит
+    convert_to_uint192(other_scale < 0 ? DEC_ZERO : overflow, value_2, &value_2_192); // конвертация числа без переполнения в 192 бит
 
-    convert_to_uint192(overflow, other_scale >= 0 ? value_2 : value_1, &with_overflow); // конвертация числа с переполнением в 192бит
-    convert_to_uint192(DEC_ZERO, other_scale < 0 ? value_1 : value_2, &without_overflow); // конвертация числа без переполнения в 192 бит 
-    my_print_192(with_overflow);
-    my_print_192(without_overflow);
-
-    // printf("%d", add_uint192(with_overflow, without_overflow, &result_add_192));
-    if (add_uint192(with_overflow, without_overflow, &result_add_192) == 0) { // сложение в 192 бит
-        int scale = get_scale(value_1);
-        printf("Степень = %d\n", scale);
-        result_function = round_result_192(&result_add_192, result, &scale); // удаление переполнениия из значения
-        printf("%d", result_function);
-        set_scale(result, scale);
+    if (sign_value_1 && sign_value_2) { // в случае отрицательных значений
+        add_uint192(value_1_192, value_2_192, &result_192);
+        set_sign(result, 1);
+    } else if (sign_value_1 && !sign_value_2) {
+        if (gt_uint192(value_1_192, value_2_192)) {
+            sub_uint192(value_1_192, value_2_192, &result_192);
+            set_sign(result, 1);
+        } else {
+           sub_uint192(value_2_192, value_1_192, &result_192);
+        }
+    } else if(!sign_value_1 && sign_value_2) {
+        if (gt_uint192(value_1_192, value_2_192)) {
+        sub_uint192(value_1_192, value_2_192, &result_192);
+        } else {
+            sub_uint192(value_2_192, value_1_192, &result_192);
+            set_sign(result, 1);
+        }
+    } else if (!sign_value_1 && !sign_value_2) { //  в случае положительных
+        add_uint192(value_1_192, value_2_192, &result_192);
     }
+
+    int scale = get_scale(value_1);
+    result_function = round_result_192(&result_192, result, &scale); // удаление переполнениия из значения
+    set_scale(result, scale);
     return result_function;
 }
 
 
 
+
+
 int main() {
-    unsigned int maxInt = 4294967295;
-    s21_decimal a = {{maxInt, maxInt, maxInt, 0}};
-    s21_decimal b = {{maxInt, maxInt, maxInt, 0}};
-    set_scale(&a, 1);
+    // unsigned int maxInt = 4294967295;
+    s21_decimal a = {{5, 0, 0, 0}};
+    s21_decimal b = {{10, 0, 0, 0}};
+    // set_scale(&b, 1);
+    // set_sign(&a, 1);
+    // set_sign(&b, 1);
     s21_decimal result = {{0}};
-    s21_add(a,b,&result);
+    s21_add(a, b, &result);
     // my_print(a);
     // my_print(b);
     // printf("%d\n", s21_add(a, b, &result));
-    // my_print(result);
+    my_print(result);
+    printf("result scale = %d", get_scale(result));
 
 
     return 0;
